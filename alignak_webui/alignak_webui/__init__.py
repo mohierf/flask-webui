da268c4c623c8a1f77215eeca5047e8868738c0f
Frédéric MOHIER <frederic.mohier@gmail.com>
2015-09-30 14:15:27 +0200

    First version
    - application starting
    - configuration file and parameters
    - log files
    - simple login / index / logout

---
+++ alignak_webui/alignak_webui/__init__.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015:
+#   Frederic Mohier, frederic.mohier@gmail.com
+#
+# This file is part of (WebUI).
+#
+# (WebUI) is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# (WebUI) is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with (WebUI).  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Web User Interface for Alignak REST backend
+"""
+from flask import Flask
+
+# Application manifest
+VERSION = (0, 1, 0)
+
+__application__ = u"Alignak_Webui"
+__version__ = '.'.join((str(each) for each in VERSION[:4]))
+__copyright__ = u"(c) 2015 - Frédéric MOHIER"
+__license__ = u"GNU AGPL version 3"
+__releasenotes__ = u"""Bootstrap 3 User Interface for Alignak backend"""
+__doc_url__ = u"https://github.com/Alignak-monitoring-contrib/alignak-webui/wiki"
+
+app = Flask(__name__.split('.')[0])
+
+settings = {
+    'DEBUG': "False",
+    'HOST': "127.0.0.1",
+    'PORT': "5000"
+}
+
+# setup.py imports this module to gather package version
+def get_version():
+    """
+    Returns shorter version (digit parts only) as string.
+    """
+    return '.'.join((str(each) for each in VERSION[:4]))
+
+# This import must always remain at the end of this file as recommended in:
+# http://flask.pocoo.org/docs/0.10/patterns/packages/
+from alignak_webui import views
---
+++ alignak_webui/alignak_webui/views.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015:
+#   Frederic Mohier, frederic.mohier@gmail.com
+#
+# This file is part of (WebUI).
+#
+# (WebUI) is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# (WebUI) is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with (WebUI).  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Main application views and routes
+"""
+from flask import Flask, session, redirect, url_for, escape, request
+from alignak_webui import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/test')
+def test():
+    return 'Test'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    else:
+        app.logger.info("Login request")
+
+        return '''
+            <form action="" method="post">
+                <p><input type=text name=username>
+                <p><input type=submit value=Login>
+            </form>
+        '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+# Shutdown Web server ...
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['GET', 'POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
